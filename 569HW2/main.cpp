6c226e9b0b787186fa36297b880f4ecab708c4ec
liuhengyue <6hengyue@gmail.com>
2015-10-01 14:38:56 -0700

    Initial Commit

---
+++ 569HW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  569HW2
+//
+//  Created by LiuHengyue on 10/1/15.
+//  Copyright © 2015 LiuHengyue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
